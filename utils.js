53611fcee6784b7d33b4495b4fdd5318608445ec
Алан Газиев <a.gaziev@bitrix.ru>
2026-01-22 14:02:41 +0200

    add reload tab with cache reset method in utils

--- utils.js
+++ utils.js
@@ -20,3 +20,30 @@ function isPortalForTest(portalUrl)
     const match = portalUrl.match(regex);
     return match != null;
 }
+
+function reloadTabBypassCache(tabId)
+{
+    const browserAPI = typeof browser !== 'undefined' ? browser : chrome;
+    if (!tabId) {
+        return Promise.reject(new Error('tabId is required'));
+    }
+
+    if (typeof browser !== 'undefined') {
+        return browserAPI.tabs.reload(tabId, { bypassCache: true });
+    }
+
+    return new Promise((resolve, reject) => {
+        try {
+            browserAPI.tabs.reload(tabId, { bypassCache: true }, () => {
+                const error = chrome.runtime?.lastError;
+                if (error) {
+                    reject(new Error(error.message));
+                } else {
+                    resolve();
+                }
+            });
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
